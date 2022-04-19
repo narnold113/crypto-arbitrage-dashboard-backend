729a1617a5b116d0502ea64b8af5367e2f68823f
Nicolas Arnold <nicolas.arnold@protonmail.com>
2022-04-19 17:17:39 -0600

    use vpc connector

--- src/config/db.js
+++ src/config/db.js
@@ -3,32 +3,44 @@ const Sequelize = require('sequelize')
 
 const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql'
 
-if (process.env.IS_CLOUD) {
-  module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
-    // host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      socketPath: `${dbSocketPath}/${process.env.PG_INSTANCE_NAME}`
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-  })
-} else {
-  module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-  })
-}
+module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+})
+
+// if (process.env.IS_CLOUD) {
+//   module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
+//     // host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       socketPath: `${dbSocketPath}/${process.env.PG_INSTANCE_NAME}`
+//     },
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     },
+//   })
+// } else {
+//   module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     dialect: 'postgres',
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     },
+//   })
+// }
 
