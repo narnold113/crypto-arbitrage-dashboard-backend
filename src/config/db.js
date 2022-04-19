67a3478d819d9598190700df8a40a543c913dd6a
Nicolas Arnold <nicolas.arnold@protonmail.com>
2022-04-19 16:58:46 -0600

    use unix socket for postgres cloud connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,34 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
-module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-})
+const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql'
+
+if (process.env.IS_CLOUD) {
+  module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
+    // host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  })
+} else {
+  module.exports =  new Sequelize('main', process.env.PG_USER, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  })
+}
+
